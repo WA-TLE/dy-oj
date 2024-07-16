9a11313f1f6b9f97ecbcae82cc5c89e78b83f2fb
dy <dyu32345@gmail.com>
2024-07-16 18:05:05 +0800

    使用代理模式加强代码沙箱功能
    请求日志, 相应日志

---
+++ src/main/java/com/dy/codesanbox/proxy/CodeSanBoxProxy.java
@@ -0,0 +1,46 @@
+package com.dy.codesanbox.proxy;
+import com.dy.model.dto.questionsubmit.JudgeInfo;
+import java.util.List;
+
+import com.dy.codesanbox.CodeSanBox;
+import com.dy.codesanbox.model.ExecuteCodeRequest;
+import com.dy.codesanbox.model.ExecuteCodeResponse;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: dy
+ * @Date: 2024/7/16 17:55
+ * @Description:
+ */
+@Slf4j
+public class CodeSanBoxProxy implements CodeSanBox {
+
+    private CodeSanBox codeSanBox;
+
+    public CodeSanBoxProxy(CodeSanBox codeSanBox) {
+        this.codeSanBox = codeSanBox;
+    }
+
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        String language = executeCodeRequest.getLanguage();
+        String code = executeCodeRequest.getCode();
+        List<String> inputList = executeCodeRequest.getInputList();
+
+        log.info("代码执行语言: {}", language);
+        log.info("用户提交代码: {}", code);
+        log.info("测试用例: {}", code);
+        ExecuteCodeResponse executeCodeResponse = codeSanBox.executeCode(executeCodeRequest);
+        String message = executeCodeResponse.getMessage();
+        List<String> outputList = executeCodeResponse.getOutputList();
+        JudgeInfo judgeInfo = executeCodeResponse.getJudgeInfo();
+        String status = executeCodeResponse.getStatus();
+
+        log.info("代码执行语言: {}", language);
+        log.info("用户代码执行信息: {}", message);
+        log.info("代码输出用例: {}", outputList);
+        log.info("判题信息: {}", judgeInfo);
+        log.info("题目执行状态: {}", status);
+        return executeCodeResponse;
+    }
+}
